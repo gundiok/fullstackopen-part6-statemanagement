a1e5d18224b163a5e9bf77bb25614b2881a62dc6
Sanjay <sansreekala@gmail.com>
2026-08-31 14:13:52 +0300

    Implemented unicafe statistics with Zustand

--- unicafe/src/components/Buttons.jsx
+++ unicafe/src/components/Buttons.jsx
@@ -1,12 +1,16 @@
+import { useScoreStore } from "../store";
+
 const Buttons = () => {
+  const { good, neutral, bad } = useScoreStore((state) => state.actions);
+
   return (
     <div>
       <h2>give feedback</h2>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={good}>good</button>
+      <button onClick={neutral}>neutral</button>
+      <button onClick={bad}>bad</button>
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
--- unicafe/src/components/Statistics.jsx
+++ unicafe/src/components/Statistics.jsx
@@ -1,26 +1,48 @@
+import { useScoreStore } from "../store";
+
 const Statistics = () => {
-  const good = 0
-  const neutral = 0
-  const bad = 0
-  const all = 0
-  const average = 0
-  const positive = 0
-  
+  const good = useScoreStore((state) => state.good);
+  const bad = useScoreStore((state) => state.bad);
+  const neutral = useScoreStore((state) => state.neutral);
+  const all = good + bad + neutral;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+
+
+
   return (
     <div>
       <h2>statistics</h2>
       <table>
         <tbody>
-          <tr><td>good</td><td>{good}</td></tr>
-          <tr><td>neutral</td><td>{neutral}</td></tr>
-          <tr><td>bad</td><td>{bad}</td></tr>
-          <tr><td>all</td><td>{all}</td></tr>
-          <tr><td>average</td><td>{average}</td></tr>
-          <tr><td>positive</td><td>{positive}</td></tr>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{all}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{ all >=1 ? average : 0 }</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{ all >=1 ? positive : 0 } % </td>
+          </tr>
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Statistics
+export default Statistics;
